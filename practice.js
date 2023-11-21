3abd7958065e4220d9d1d0ea3ebe8b316612508f
cyuzuzo0699 <cyuzuzoyvette0699@gmail.com>
2023-11-21 16:58:44 +0200

    my js pratices

---
+++ practice.js
@@ -0,0 +1,12 @@
+console.log('hello world');
+//declaration
+let num = 1;
+num = 2;
+console.log(num);
+
+const num = 6;
+console.log(num);
+
+//strings declaration
+myName = "Cyuzuzo"
+myStr = "My name is " + myName + " and I am well!"
