ca6e9426c0544fc3abe3c381a3b87a4d0cb96cf7
cyuzuzo0699 <cyuzuzoyvette0699@gmail.com>
2023-11-22 16:34:15 +0200

    day 2 practices

---
+++ day2/day2.js
@@ -0,0 +1,18 @@
+const isTrue = true;
+console.log(isTrue);
+//
+console.log(Boolean(100));
+//
+const priceOne = 5;
+const priceTwo = 10;
+console.log(priceOne == priceTwo);
+//
+const pricene = 10;
+const pricewo = 10;
+console.log(pricene == pricewo);
+//
+const pricOne = 5;
+const pricTwo = 10;
+ 
+console.log(pricOne < pricTwo);
+console.log(pricTwo < pricOne);
