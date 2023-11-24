fdcd5694880abf0cadedb2fa8f2c2e1cafa5ca01
cyuzuzo0699 <cyuzuzoyvette0699@gmail.com>
2023-11-24 09:12:59 +0200

    day3 practices

---
+++ day3/day3.js
@@ -0,0 +1,23 @@
+/*let text = "hello";
+console.log(text);
+text = 'hi';
+console.log(text);
+text = `bye`;
+console.log(text);
+//
+const textOne = 'I am';
+const textTwo = "Cyuzuzo";
+const textThree = `Yvette`;
+console.log(textOne, textTwo, textThree);
+//conditions
+
+let text = 'hello';
+if (1>=0) {
+   text = text + ' world';
+}
+console.log(text === 'hello world');*/
+//
+const arr=[1,2,3,4,5];
+arr.forEach(function(val)){
+document.getElementById("demo").innerHtml=val;
+}
