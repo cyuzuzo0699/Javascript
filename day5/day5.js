224c2bf4b7b6941990621be764c0e3272c11df4d
cyuzuzo0699 <cyuzuzoyvette0699@gmail.com>
2023-11-27 16:36:44 +0200

    day5practice

---
+++ day5/day5.js
@@ -0,0 +1,18 @@
+let  counter;
+ console.log(counter); 
+{
+       counter  =  1;
+        {
+                console.log(counter);  
+        }
+ }
+counter  =  counter  +  1;
+ console.log(counter);  
+
+// let  counter  =  100;
+// 2 console.log(counter);  //  ->  100
+// 3 {
+// 4        counter  =  200;
+// 5        console.log(counter);  //  ->  200
+// 6 }
+// 7 console.log(counter);  //  ->  200
