d5c48e20d587d62acee36c1752793f7c9ca71da1
cyuzuzo0699 <cyuzuzoyvette0699@gmail.com>
2023-11-28 16:42:31 +0200

    day6practices

---
+++ day6/lab.js
@@ -0,0 +1,11 @@
+const name1 = "Maxwell Wright";
+const phone1 = '(0191) 719 6495';
+const email1 = 'Curabitur.egestas.nunc@nonummyac.co.uk';
+const name2 = 'Raja Villarreal';
+const phone2 = '0866 398 2895';
+const email2 = 'posuere.vulputate@sed.com';
+const name3 = 'Helen Richards';
+const phone3 = '0800 1111';
+const email3 = 'libero@convallis.edu';
+console.log(name1,'/',phone1,'/',email1);
+console.log(name3,'/',phone3,'/',email3);
