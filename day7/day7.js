1dd787e720b946c3309e03c92f4eb18fe8767895
cyuzuzo0699 <cyuzuzoyvette0699@gmail.com>
2023-12-01 09:18:44 +0200

    day7practices

---
+++ day7/day7.js
@@ -0,0 +1,12 @@
+ let  year  =  1990;
+ console.log(year);  
+ console.log(1991);  
+ console.log("Alice");  
+//number datatype
+let a= 1/0;
+console.log(a);
+let s= "helloo";
+let n= s*2;
+console.log(n);
+let path= "C\\\\windows";
+console.log(path);
